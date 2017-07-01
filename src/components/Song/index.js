7277ad9649095260a92d6621dc5917cf23d5af98
Scott Blumenthal <scott.blumenthal@gmail.com>
2017-07-01 18:45:04 -0400

    toggle chords

--- src/components/Song/index.js
+++ src/components/Song/index.js
@@ -4,6 +4,7 @@ import styles from './styles.scss';
 import Chart from '../Chart'
 import $ from 'jquery'
 import archieml from 'archieml'
+import marked from 'marked'
 import { CHORDS } from '../../util/chords'
 
 class Song extends React.Component {
@@ -12,7 +13,8 @@ class Song extends React.Component {
     super(props)
     this.state = {
       title: '',
-      song: []
+      song: [],
+      visibleCharts: []
     }
   }
 
@@ -26,15 +28,35 @@ class Song extends React.Component {
     })
   }
 
-  componentDidUpdate() {
-    console.log(this.state);
+  toggleChordChart(chord) {
+    let current = this.state.visibleCharts.slice(),
+        nowAt = current.indexOf(chord)
+
+    if (nowAt != -1) {
+      current.splice(nowAt, 1)
+    } else {
+      current.push(chord)
+    }
+    console.log(current);
+    this.setState({ visibleCharts: current })
   }
 
-  renderSection({ section, chords, text }, i) {
+  renderSection({ section, chords, words }, i) {
+    let { visibleCharts } = this.state
+
     return <div className="section" key={ `song-section-${i}`}>
       <header>{ section }</header>
       <div className="chords">
+        { chords.split(',').map((c) => {
+            return <span className={ styles.chordname } onClick={ () => this.toggleChordChart(c) }>{ c.trim()}</span>
+        }) }
+      </div>
+      <div className={ styles.words } dangerouslySetInnerHTML={ {__html: marked(words)}} />
+      <div className="chords-charts">
         { chords.split(',').map((c, i) => {
+          if (visibleCharts.indexOf(c) == -1) {
+            return null
+          }
           let chord = c.trim()
           return <Chart name={ chord } keys={ CHORDS[chord.toLowerCase()] } key={ `song-chord-${i}`} />
         }) }
