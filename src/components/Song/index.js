a9b555363c3d67d6a1a2909a213f826ad19467c0
Scott Blumenthal <scott.blumenthal@gmail.com>
2017-07-01 15:37:30 -0400

    figured out song format and wired it up

--- src/components/Song/index.js
+++ src/components/Song/index.js
@@ -2,25 +2,59 @@
 import React from 'react';
 import styles from './styles.scss';
 import Chart from '../Chart'
+import $ from 'jquery'
+import archieml from 'archieml'
 
-const CHORDS = [
-  {
-    name: 'badd9',
-    keys: ['b','ds','fs','cs']
-  }
-]
+const CHORDS = {
+  'badd9': ['b','ds','fs','cs'],
+  'f#madd9/a': ['fs','as','cs','ds'],
+  'asus2': ['a','b','e'],
+  'e': []
+}
 
 class Song extends React.Component {
 
-  componentDidMount() {
-    
+  constructor(props) {
+    super(props)
+    this.state = {
+      title: '',
+      song: []
+    }
   }
 
-  render () {
-    console.log(this.props);
+  componentDidMount() {
+    $.ajax({
+      url: `/songs/${this.props.routeParams.id}.aml`,
+      dataType: 'text'
+    }).then((aml) => {
+      let song = archieml.load(aml);
+      this.setState(Object.assign({}, this.state, song))
+    })
+  }
+
+  componentDidUpdate() {
+    console.log(this.state);
+  }
+
+  renderSection({ section, chords, text }, i) {
+    return <div className="section" key={ `song-section-${i}`}>
+      <header>{ section }</header>
+      <div className="chords">
+        { chords.split(',').map((c, i) => {
+          let chord = c.trim()
+          return <Chart name={ chord } keys={ CHORDS[chord.toLowerCase()] } key={ `song-chord-${i}`} />
+        }) }
+      </div>
+    </div>
+  }
+
+  render() {
+    let { title, song } = this.state
+
     return <div className={ styles.song }>
-      { CHORDS.map((c, i) => {
-        return <Chart name={ c.name } keys={ c.keys } key={ `song-chord-${i}`} />
+      <header>{ title }</header>
+      { song.map((section, i) => {
+        return this.renderSection(section, i)
       }) }
     </div>
   }
